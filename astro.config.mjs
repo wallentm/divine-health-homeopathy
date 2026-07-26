8cddc28e56ae56b314105a1176e791857d20666b
wallentm <mindy@teamwallent.com>
2026-07-26 19:26:12 -0400

    Use www as canonical site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://divinehealthhomeopathy.com',
+  site: 'https://www.divinehealthhomeopathy.com',
   integrations: [sitemap()],
 });
